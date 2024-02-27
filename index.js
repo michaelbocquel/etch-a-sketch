fe4e560d64b6c3cff8cc02db587a7e192ec73a79
nosca <michael.bocquel@gmail.com>
2024-02-27 10:49:34 +0100

    Add button to toggle borders on and off, add input to change pen color, update function resetGrid, separate function to create grid and grid cells, add a text display to visualize grid size, update function to change cells background color

--- index.js
+++ index.js
@@ -1,32 +1,86 @@
 let gridSize
 
+let isBorders = true
+let borderDisplay = document.querySelector('.border-display')
+borderDisplay.textContent = `Borders : On`
+
+let penColor = document.querySelector('#pen-color')
+
+allGridCells = document.querySelectorAll('.grid-cell')
+
 let gridContainer = document.querySelector('.grid-container')
 gridContainer.style.display = `grid`
 
 let gridSizeInput = document.querySelector('#grid-size-input')
 gridSizeInput.addEventListener('change', () => {
-	createGrid(gridSizeInput.value)
+	resetGrid()
+})
+
+gridSizeInput.addEventListener('input', () => {
+	gridSizeDisplay.textContent = `${gridSizeInput.value} x ${gridSizeInput.value}`
+})
+
+let gridSizeDisplay = document.querySelector('.grid-size-display')
+gridSizeDisplay.textContent = `${gridSizeInput.value} x ${gridSizeInput.value}`
+
+function isBordersToggle() {
+	isBorders = !isBorders
+}
+let toggleCellBordersButton = document.querySelector('.toggle-cell-borders-button')
+
+toggleCellBordersButton.addEventListener('click', () => {
+	isBordersToggle()
+	if (isBorders === true) {
+		borderDisplay.textContent = `Borders : On`
+	} else {
+		borderDisplay.textContent = `Borders : Off`
+	}
+	allGridCells.forEach((element) => {
+		if (isBorders === true) {
+			element.style.border = `1px solid var(--lightblack)`
+		} else {
+			element.style.border = `none`
+		}
+	})
 })
 
 let resetGridButton = document.querySelector('.reset-grid-button')
 
 resetGridButton.addEventListener('click', () => {
-	resetGrid
-	createGrid(gridSizeInput.value)
+	resetGrid()
 })
 
 function createGrid(gridSize) {
-	resetGrid()
 	gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
 	gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+}
+
+function createGridCells(gridSize, isBorders) {
 	for (let i = 0; i < gridSize ** 2; i++) {
 		newGridCell = document.createElement('div')
 		newGridCell.style.backgroundColor = `black`
-		newGridCell.style.border = `1px solid var(--lightblack)`
+		newGridCell.classList.add('grid-cell')
+		if (isBorders === true) {
+			newGridCell.style.border = `1px solid var(--lightblack)`
+		} else {
+			newGridCell.style.border = `none`
+		}
 		gridContainer.append(newGridCell)
 	}
+	allGridCells = document.querySelectorAll('.grid-cell')
+	allGridCells.forEach((element) => {
+		element.addEventListener('mouseover', () => {
+			element.style.backgroundColor = `${penColor.value}`
+		})
+	})
 }
+
 
 function resetGrid() {
 	gridContainer.textContent = ''
+	createGrid(gridSizeInput.value, isBorders)
+	createGridCells(gridSizeInput.value, isBorders)
 }
+
+createGrid(gridSizeInput.value, isBorders)
+createGridCells(gridSizeInput.value, isBorders)
